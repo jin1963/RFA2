001d04ad6ee95e324107e7676681528843fe0529
jin1963 <kaojino89@gmail.com>
2025-07-29 14:50:02 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+const contractAddress = "0xC444F117806B725E12154Fa7D0cd090Eec325B48";
+const kjcAddress = "0xd479ae350dc24168e8db863c5413c35fb2044ecd";
+const usdtAddress = "0x55d398326f99059fF775485246999027B3197955";
+const routerAddress = "0x10ED43C718714eb63d5aA57B78B54704E256024E";
+const chainId = 56; // BNB Smart Chain Mainnetp
