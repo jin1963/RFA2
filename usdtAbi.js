1eab9cc73adc3af662f133925aadf5517d17eb9d
jin1963 <kaojino89@gmail.com>
2025-07-29 14:46:50 +0700

    Create usdtAbi.js

---
+++ usdtAbi.js
@@ -0,0 +1,19 @@
+const usdtABI = [
+  {
+    constant: false,
+    inputs: [
+      { name: "spender", type: "address" },
+      { name: "value", type: "uint256" }
+    ],
+    name: "approve",
+    outputs: [{ name: "", type: "bool" }],
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [{ name: "account", type: "address" }],
+    name: "balanceOf",
+    outputs: [{ name: "", type: "uint256" }],
+    type: "function"
+  }
+];
