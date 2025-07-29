02191e05c5b21e52ad6ae7dc463ff766e99245c3
jin1963 <kaojino89@gmail.com>
2025-07-29 14:47:41 +0700

    Create style.js

---
+++ style.js
@@ -0,0 +1,25 @@
+body {
+  background-color: #000;
+  color: gold;
+  font-family: Arial, sans-serif;
+  text-align: center;
+  padding: 20px;
+}
+
+input, button {
+  padding: 10px;
+  margin: 5px;
+  font-size: 16px;
+}
+
+button {
+  background-color: gold;
+  color: black;
+  border: none;
+  cursor: pointer;
+}
+
+#status {
+  margin-top: 15px;
+  font-weight: bold;
+}
